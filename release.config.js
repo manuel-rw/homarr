feb46de8264fc99cddd79e77fa7939b7728cfc2a
Manuel <30572287+manuel-rw@users.noreply.github.com>
2024-12-15 21:48:09 +0100

    config: add semantic release

---
+++ release.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  branches: [
+    "master",
+    {
+      name: "dev",
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "dist/**"],
+        message: "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
